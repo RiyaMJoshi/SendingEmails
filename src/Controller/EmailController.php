6f3af0744695ec6d1495b09aab81984983e127f0
Riya Joshi <riyajoshi312@gmail.com>
2022-04-19 18:53:22 +0530

    Email Notifications Added

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Notifier\NotifierInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\Recipient\Recipient;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mime\Address;
@@ -15,7 +18,7 @@ class EmailController extends AbstractController
     /**
      * @Route("/email")
      */
-    public function sendEmail(MailerInterface $mailer, $appEmail, $projectDir, $publicDir)
+    public function sendEmail(MailerInterface $mailer, $appEmail, $projectDir, $publicDir, NotifierInterface $notifier)
     {
         // $email = (new Email())
         //     ->from('riyajoshi312@gmail.com')
@@ -24,9 +27,10 @@ class EmailController extends AbstractController
         //     ->text('Thanks for trusting us!')
         //     ->html('<p>Thank you, your order has been placed</p>');
 
+        $to = 'riyajoshi.190673107028@gmail.com';
         $email = (new TemplatedEmail())
             ->from($appEmail)
-            ->to(new Address('riyajoshi.190673107028@gmail.com', 'Riya'))
+            ->to(new Address($to, 'Riya'))
             ->subject('Your order has been placed')
             ->textTemplate('emails/order-confirmation.txt.twig')
             //->htmlTemplate('emails/order-confirmation.html.twig')
@@ -41,8 +45,40 @@ class EmailController extends AbstractController
 
         $mailer->send($email);
 
+        // Create a Notification that has to be sent
+        // using the "email" channel
+        $notification = (new Notification('New Invoice', ['email']))
+            ->content('You got a new invoice for 15 EUR.');
+
+        // The receiver of the Notification
+        $recipient = new Recipient($to);
+
+        // Send the notification to the recipient
+        $notifier->send($notification, $recipient);
+
         return new Response('Email sent');
+        //return $this->redirectToRoute('notify_user');
     }
+
+    /**
+     * @Route("/email/notification", name="notify_user")
+     */
+    public function notifyUser(NotifierInterface $notifier)
+    {
+        // // Create a Notification that has to be sent
+        // // using the "email" channel
+        // $notification = (new Notification('New Invoice', ['email']))
+        //     ->content('You got a new invoice for 15 EUR.');
+
+        // // The receiver of the Notification
+        // $recipient = new Recipient(
+        //     $user->getEmail()
+        // );
+
+        // // Send the notification to the recipient
+        // $notifier->send($notification, $recipient);
+    }
+
 
     /**
      * @Route("/home", name="app_home")
