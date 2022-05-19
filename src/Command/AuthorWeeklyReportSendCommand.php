624da001a7fd66cfd947bd1b904b007fdfbc8ccf
Riya Joshi <riyajoshi312@gmail.com>
2022-05-19 17:31:51 +0530

    Generating and Attaching Styled PDF to Email

--- src/Command/AuthorWeeklyReportSendCommand.php
+++ src/Command/AuthorWeeklyReportSendCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use Knp\Snappy\Pdf;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -11,6 +12,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupInterface;
+use Twig\Environment;
 
 #[AsCommand(
     name: 'app:author-weekly-report:send',
@@ -19,12 +22,19 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class AuthorWeeklyReportSendCommand extends Command
 {
     private $mailer;
+    private $twig;
+    private $pdf;
+    private $entrypointLookup;
 
-    public function __construct(MailerInterface $mailer)
+    public function __construct(MailerInterface $mailer, Environment $twig, Pdf $pdf, EntrypointLookupInterface $entrypointLookup)
     {
         parent::__construct(null);
         $this->mailer = $mailer;
+        $this->twig = $twig;
+        $this->pdf = $pdf;
+        $this->entrypointLookup = $entrypointLookup;
     }
+
     protected function configure(): void
     {
         $this
@@ -37,11 +47,19 @@ class AuthorWeeklyReportSendCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
              
+        $this->entrypointLookup->reset();
+        
+        $html = $this->twig->render('emails/author-weekly-report-pdf.html.twig');
+        $pdf = $this->pdf->getOutputFromHtml($html);
+       
         $email = (new TemplatedEmail())
                 ->from('riyajoshi312@gmail.com')
                 ->to('riyajoshi.190673107028@gmail.com')
                 ->subject('Your weekly report on the Space Bar!')
-                ->htmlTemplate('emails/author-weekly-report.html.twig');
+                ->htmlTemplate('emails/author-weekly-report.html.twig')
+                ->attach($pdf, sprintf('weekly-report-%s.pdf', date('Y-m-d')))
+            ;
+            
             $this->mailer->send($email);
         
     
